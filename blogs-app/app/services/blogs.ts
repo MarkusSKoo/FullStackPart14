ffac5e9f162515970c9ba4fea898e82b32879856
MarkusSKoo <markus.kauhanen@helsinki.fi>
2026-08-25 14:25:49 +0300

    Fix type script problem in services/blogs

--- blogs-app/app/services/blogs.ts
+++ blogs-app/app/services/blogs.ts
@@ -1,6 +1,6 @@
-import { eq, ilike } from "drizzle-orm";
+import { eq, ilike, sql } from "drizzle-orm";
 import { db } from "../../db";
-import { blogs } from "../../db/schema";
+import { blogs, users } from "../../db/schema";
 
 export const getBlogs = async (filter?: string) => {
   if (filter) {
@@ -31,5 +31,10 @@ export const addBlog = async (title: string, author: string, url: string) => {
   const user = await db.query.users.findFirst({
     orderBy: sql`RANDOM()`,
   });
+
+  if (!user) {
+    throw new Error("User not found");
+  }
+
   await db.insert(blogs).values({ title, author, url, userId: user.id });
 };
