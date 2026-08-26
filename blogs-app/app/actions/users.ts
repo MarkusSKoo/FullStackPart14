e3bf1d6d03a90cac4c08411f92d246ab3bccaa08
MarkusSKoo <markus.kauhanen@helsinki.fi>
2026-08-26 09:11:59 +0300

    Import revalidate

--- blogs-app/app/actions/users.ts
+++ blogs-app/app/actions/users.ts
@@ -1,6 +1,7 @@
 "use server";
 
 import { redirect } from "next/navigation";
+import { revalidatePath } from "next/cache";
 import bcrypt from "bcryptjs";
 import { db } from "@/db";
 import { users } from "@/db/schema";
