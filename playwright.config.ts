eb5136e86cb66895c3ef05b6ab7f00bc64904300
MarkusSKoo <markus.kauhanen@helsinki.fi>
2026-08-28 19:02:22 +0300

    Start application in playwright config

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,8 +1,14 @@
-import { defineConfig } from "@playwright/test"
+import { defineConfig } from "@playwright/test";
 
 export default defineConfig({
   testDir: "./tests",
+
+  webServer: {
+    command: "npm run dev",
+    url: "http://localhost:3000",
+  },
+
   use: {
     baseURL: "http://localhost:3000",
   },
-})
+});
